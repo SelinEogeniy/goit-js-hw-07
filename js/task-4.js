6a5c3fd5a1af85300edaba9bf0864b598c656345
SelinEogeniy <seljax92@gmail.com>
2025-07-07 02:36:42 +0300

    -finish-hw-07

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,23 @@
+const refs = {
+  form: document.querySelector('.login-form'),
+};
 
+const onFormSubmit = event => {
+  event.preventDefault();
+  console.log("Form submitted");
+
+  const formData = {
+    email: refs.form.elements.email.value.trim(),
+    password: refs.form.elements.password.value.trim(),
+  }
+
+  if (Object.values(formData).includes('')) {
+    alert ('All form fields must be filled in');
+    return;
+  }
+
+  console.log(formData);
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+  
